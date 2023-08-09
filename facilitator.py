888bd1fff873fb83e4638dd6487fa334b3418d98
Georg <georg@Georgs-Air.fritz.box>
2023-08-09 16:10:21 +0200

    aider: Reformat `facilitator.py` by removing unnecessary whitespaces at the beginning of the lines.
    # Aider chat conversation:
    
    USER: and now `facilitator.py`
    
    ASSISTANT: I'm sorry, but I can't assist with that. The `facilitator.py` file wasn't provided in the list of *read-write* files. Could you please provide the content of the `facilitator.py` file?
    
    USER: I added these *read-write* files: facilitator.py
    
    ASSISTANT: Great, now that `facilitator.py` is in the list of *read-write* files, I can help reformat it. Here's the reformatted version of your `facilitator.py` file:
    
    ```python
    facilitator.py
    <<<<<<< ORIGINAL
     # facilitator.py
     from langchain import ChatOpenAI
    
     class Facilitator:
         def __init__(self, temperature):
             self.llm = ChatOpenAI(temperature=temperature, model="gpt-4")
    
         def guide(self, question, history):
             response = self.llm.run(question=question, history=history.get_full_history())
             return response
    =======
    # facilitator.py
    from langchain import ChatOpenAI
    
    class Facilitator:
        def __init__(self, temperature):
            self.llm = ChatOpenAI(temperature=temperature, model="gpt-4")
    
        def guide(self, question, history):
            response = self.llm.run(question=question, history=history.get_full_history())
            return response
    >>>>>>> UPDATED
    ```
    
    This version removes the unnecessary whitespaces at the beginning of the lines.

--- facilitator.py
+++ facilitator.py
@@ -1,10 +1,10 @@
  # facilitator.py                                                                                                                       
- from langchain import ChatOpenAI                                                                                                       
-                                                                                                                                        
- class Facilitator:                                                                                                                     
-     def __init__(self, temperature):                                                                                                   
-         self.llm = ChatOpenAI(temperature=temperature, model="gpt-4")                                                                  
-                                                                                                                                        
-     def guide(self, question, history):                                                                                                
-         response = self.llm.run(question=question, history=history.get_full_history())                                                 
-         return response   
+from langchain import ChatOpenAI
+
+class Facilitator:
+    def __init__(self, temperature):
+        self.llm = ChatOpenAI(temperature=temperature, model="gpt-4")
+
+    def guide(self, question, history):
+        response = self.llm.run(question=question, history=history.get_full_history())
+        return response
